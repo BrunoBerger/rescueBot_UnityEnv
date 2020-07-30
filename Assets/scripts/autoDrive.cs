fd751746642d5e1a3645edbd0b4b76cba55d9811
BrunoBerger <bruno.berger@stud.hshl.de>
2020-07-30 16:04:01 +0200

    bot now turns towards a beacon

---
+++ Assets/scripts/autoDrive.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoDrive : MonoBehaviour
+{
+    public float maxSpeed = 20f;
+    private Vector3 turnSpeed;
+    public Rigidbody rb;
+    private bool isgrounded;
+
+    public GameObject curBeacon;
+    public Transform target;
+
+    private readonly VectorPid angularVelocityController = new VectorPid(33.7766f, 0, 0.2553191f);
+    private readonly VectorPid headingController = new VectorPid(9.244681f, 0, 0.06382979f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = this.GetComponent<Rigidbody>();
+      curBeacon = GameObject.Find("beacon1");
+      turnSpeed= new Vector3(0, 5, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      target = curBeacon.transform;
+    }
+    void FixedUpdate()
+    {
+      if (isgrounded==true)
+      {
+        var angularVelocityError = rb.angularVelocity * -1;
+        Debug.DrawRay(transform.position, rb.angularVelocity * 10, Color.black);
+
+        var angularVelocityCorrection = angularVelocityController.Update(angularVelocityError, Time.deltaTime);
+        Debug.DrawRay(transform.position, angularVelocityCorrection, Color.green);
+
+        rb.AddTorque(angularVelocityCorrection);
+
+        var desiredHeading = target.position - transform.position;
+        Debug.DrawRay(transform.position, desiredHeading, Color.magenta);
+
+        var currentHeading = transform.forward;
+        Debug.DrawRay(transform.position, currentHeading * 15, Color.blue);
+
+        var headingError = Vector3.Cross(currentHeading, desiredHeading);
+        var headingCorrection = headingController.Update(headingError, Time.deltaTime);
+
+        rb.AddTorque(headingCorrection);
+      }
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+      if (collisionInfo.gameObject.name == "Terrain")
+      {
+        isgrounded = true;
+      }
+    }
+    void OnCollisionExit(Collision collisionInfo)
+    {
+      if (collisionInfo.gameObject.name == "Terrain")
+      {
+        isgrounded = false;
+      }
+    }
+}
+
+public class VectorPid
+{
+    public float pFactor, iFactor, dFactor;
+
+    private Vector3 integral;
+    private Vector3 lastError;
+
+    public VectorPid(float pFactor, float iFactor, float dFactor)
+    {
+        this.pFactor = pFactor;
+        this.iFactor = iFactor;
+        this.dFactor = dFactor;
+    }
+
+    public Vector3 Update(Vector3 currentError, float timeFrame)
+    {
+        integral += currentError * timeFrame;
+        var deriv = (currentError - lastError) / timeFrame;
+        lastError = currentError;
+        return currentError * pFactor
+            + integral * iFactor
+            + deriv * dFactor;
+    }
+}
+
+// PID-Stuff from
+// https://answers.unity.com/questions/199055/addtorque-to-rotate-rigidbody-to-look-at-a-point.html
--- Assets/scripts/cubeMovement.cs
+++ Assets/scripts/cubeMovement.cs
@@ -4,39 +4,32 @@ using UnityEngine;
 
 public class cubeMovement : MonoBehaviour
 {
-    public bool isgrounded;
     public float force = 20f;
     public float torque;
-    public float maxSpeed = 40f;
-    private float potGain;
+    public float maxSpeed = 20f;
 
     public Rigidbody rb;
     public Vector3 velocity;
+    private float potGain;
+    public bool isgrounded;
+
     public Vector3 inputDrive;
     public float inputTurn;
-    // public GameObject myCam;
-    // private Vector3 myCamAngle;
-    // private cameraFollow myCamScript;
-    // Start is called before the first frame update
+
+
     void Start()
     {
-      torque = 1;
+      torque = 3f;
       rb = this.GetComponent<Rigidbody>();
-      // myCam = GameObject.Find("Main Camera");
-      // myCamScript = myCam.GetComponent<cameraFollow>();
-      // myCamAngle = myCamScript.camAngle;
-    }
 
+    }
     // Update is called once per frame
     void Update()
     {
-      // myCamAngle = myCamScript.camAngle;
       inputDrive = new Vector3(0f,0f,Input.GetAxis("Vertical"));
       inputTurn = Input.GetAxis("Horizontal");
       potGain = rb.velocity.magnitude + inputDrive.magnitude;
-      // input = Vector3.Scale(input, myCamAngle);
     }
-
     void FixedUpdate()
     {
       if (isgrounded==true)
@@ -49,10 +42,13 @@ public class cubeMovement : MonoBehaviour
       }
     }
 
+
     void moveChar(Vector3 direction)
     {
       rb.AddRelativeForce(direction * force);
     }
+
+
 
     void OnCollisionEnter(Collision collisionInfo)
     {
@@ -63,7 +59,7 @@ public class cubeMovement : MonoBehaviour
     }
     void OnCollisionExit(Collision collisionInfo)
     {
-      if (collisionInfo.gameObject.name == "floor")
+      if (collisionInfo.gameObject.name == "Terrain")
       {
         isgrounded = false;
       }
